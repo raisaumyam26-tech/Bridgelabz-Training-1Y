491d517ac1cb7a430d59619472c198b236df46b4
Saumyam Rai <raisaumyam26@gmail.com>
2026-02-10 23:12:21 +0530

    level 1

--- String-API/src/com/gla/String/NullPointerDemo.java
+++ String-API/src/com/gla/String/NullPointerDemo.java
@@ -17,7 +17,6 @@ public class NullPointerDemo {
     }
 
     public static void main(String[] args) {
-        // generateException(); // Uncomment to see exception
         handleException();
     }
 }
